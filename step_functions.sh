ab09848f71dd61a4ca361f46e8a5453c35e51b62
ma duce <maduce@tormail.org>
2013-12-14 07:35:36 -0800

    Updating step_functions.sh - combining scramble and descramble functions into single Coordinate_Scramble function.  Adding progress indicator.

--- step_functions.sh
+++ step_functions.sh
@@ -6,29 +6,50 @@ awk '/HEADER;/,/FILE_SCHEMA/' $file
 
 }
 
-function graball_Coordinate_Points () {
+function graball_Coordinate_Points() {
 local file=$1
 
 awk '/DATA/,/Failed/' $file |grep CARTESIAN_POINT | sed 's/[^ ]* //' | sed 's/[^ ]* //' | sed 's/[^ ]* //' | sed 's/[^ ]* //' | sed 's/[^ ]* //'
 
 }
 
-function Coordinate_xyz_Scramble() {
+function Coordinate_Scramble() {
 
-local filetemp=$1
+local scrambleopt=$1
 local modulo_factor=$2
+local filetemp=$3
+local row_total=$(graball_Coordinate_Points $filetemp |wc -l)
 local string1="CARTESIAN_POINT" 
-local output=$(echo output.step)
 rm -f $output
+
+# check if user is scrambling or descrambling
+case "$scrambleopt" in
+    scramble)
+            local output=$(echo output.step)
+            local scramblesign=$(echo +)
+            echo
+            echo "* Scrambling $row_total lines in $filetemp. Saving as $output"
+            ;;
+         
+    descramble)
+            local output=$(echo original.step)
+            local scramblesign=$(echo -)
+            echo
+            echo "* Descrambling $row_total lines in  $filetemp. Saving as $output..."
+
+            ;;
+    *)
+            exit 1
+            ;;
+esac
 
 while read line           
 do           
-    # echo $line |awk '{print $3}'           
     local string_temp=$(echo $line |awk '{print $3}');
 
 case "$string_temp" in
     $string1)  
-            #changing coordinates
+            #grabing coordinates
             local x_coordinate=$(echo $line |awk '{print $7}' | cut -d "," -f 1)
             local y_coordinate=$(echo $line |awk '{print $8}' | cut -d "," -f 1)
             local z_coordinate=$(echo $line |awk '{print $9}' | cut -d "," -f 1)
@@ -36,21 +57,25 @@ case "$string_temp" in
             #local sign_exponent=$(echo "$row_number % 2" |bc)
             local row_number_mod3=$(echo "$row_number % 3" |bc)
             local scramble_term=$(echo "$row_number % $modulo_factor" |bc)
+            local progress=$(echo "scale = 2 ; $row_number*100/$row_total" |bc)
 
             case $row_number_mod3 in 
                 0)
-                 echo "$row_number is 0"
-                 echo $line |sed "s/$x_coordinate/$(echo $x_coordinate + $scramble_term | bc)/g" >>$output
+                 echo $line |sed "s/$x_coordinate/$(echo $x_coordinate $scramblesign $scramble_term | bc)/g" >>$output
+                 echo -n "  [:)  ] $progress% finished."
+                 echo -n R | tr 'R' '\r'
                  ;;
 
                 1)
-                 echo "$row_number is 1"
-                 echo $line |sed "s/$y_coordinate/$(echo $y_coordinate + $scramble_term | bc)/g" >>$output
+                 echo $line |sed "s/$y_coordinate/$(echo $y_coordinate $scramblesign $scramble_term | bc)/g" >>$output
+                 echo -n "  [ :) ] $progress% finished."
+                 echo -n R | tr 'R' '\r'
                  ;;
 
                 2)
-                 echo "$row_number is 2"
-                 echo $line |sed "s/$z_coordinate/$(echo $z_coordinate + $scramble_term | bc)/g" >>$output
+                 echo $line |sed "s/$z_coordinate/$(echo $z_coordinate $scramblesign $scramble_term | bc)/g" >>$output
+                 echo -n "  [  :)] $progress% finished."
+                 echo -n R | tr 'R' '\r'
                  ;;
             esac
             ;;
@@ -62,55 +87,6 @@ esac
 done <$filetemp
 }
 
-function Coordinate_xyz_unScramble() {
-
-local filetemp=$1
-local modulo_factor=$2
-local string1="CARTESIAN_POINT" 
-local output=$(echo original	.step)
-rm -f $output
-
-while read line           
-do           
-    # echo $line |awk '{print $3}'           
-    local string_temp=$(echo $line |awk '{print $3}');
-
-case "$string_temp" in
-    $string1)  
-            #changing coordinates
-            local x_coordinate=$(echo $line |awk '{print $7}' | cut -d "," -f 1)
-            local y_coordinate=$(echo $line |awk '{print $8}' | cut -d "," -f 1)
-            local z_coordinate=$(echo $line |awk '{print $9}' | cut -d "," -f 1)
-            local row_number=$(echo $line |awk '{print $1}' |cut -d "#" -f 2);
-            #local sign_exponent=$(echo "$row_number % 2" |bc)
-            local row_number_mod3=$(echo "$row_number % 3" |bc)
-            local scramble_term=$(echo "$row_number % $modulo_factor" |bc)
-
-            case $row_number_mod3 in 
-                0)
-                 echo "$row_number is 0"
-                 echo $line |sed "s/$x_coordinate/$(echo $x_coordinate - $scramble_term | bc)/g" >>$output
-                 ;;
-
-                1)
-                 echo "$row_number is 1"
-                 echo $line |sed "s/$y_coordinate/$(echo $y_coordinate - $scramble_term | bc)/g" >>$output
-                 ;;
-
-                2)
-                 echo "$row_number is 2"
-                 echo $line |sed "s/$z_coordinate/$(echo $z_coordinate - $scramble_term | bc)/g" >>$output
-                 ;;
-            esac
-            ;;
-            
-           *) 
-            echo $line >> $output
-            ;;
-esac
-done <$filetemp
-}
-
-## test. You can change 3 to any integer you desire.
-#Coordinate_xyz_Scramble $1 3
-#Coordinate_xyz_unScramble $1 3
+## test - Note you must use same number to unscramble.
+# Coordinate_Scramble <scramble|descramble> <number> file.step
+Coordinate_Scramble $1 $2 $3
